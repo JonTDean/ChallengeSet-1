209f2d404a0bb96fa62bd3e3206a77fa558e8903
JonTDean <JonathanTomDean@Gmail.com>
2021-01-27 11:50:51 -0500

    Basic Setup Complete

---
+++ EvenNums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/* Challenge - Even Numbers
+ * Task - 
+ *  1. Write a method named even_nums() 
+ *     with the parameter <Integer :: max >
+ *  
+ *  2. The Method must Return a collection of 
+ *      integers containing all even numbers
+ *      from 0 to <Integer :: max >
+*/
+ 
+
+namespace EvenNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare Method Here.
+        }
+
+        // Write Method Here.
+    }
+}
---
+++ EvenNumsTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EvenNumsTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
